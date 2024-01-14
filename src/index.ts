3a54d094d4252a9b483763646af218226e755c0c
andyrhman <andyrahman989@gmail.com>
2024-01-14 09:09:45 +0800

    Added mongoose configuration

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 import "reflect-metadata"
 import express from "express";
-import { myDataSource } from "./config/db.config";
+import mongoose from "mongoose";
 import { routes } from "./routes";
 
 const app = express();
 app.use(express.json());
 
-// Initialize TypeORM connection and start the Express server
-myDataSource.initialize().then(() => {
-    routes(app);
+routes(app);
 
-    console.log("Database has been initialized!");
-    app.listen(8000, () => {
-        console.log('Server listening on port 8000');
-    });
-}).catch((err) => {
-    console.error("Error during Data Source initialization:", err);
+mongoose.connect('mongodb://localhost/node_admin')
+    .then(() => console.log('Database has been initialized!'))
+    .catch((err) => console.error(err));
+
+app.listen(8000, () => {
+    console.log('Server listening on port 8000');
 });
